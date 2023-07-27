1a599330f306304a171ffedb116834e7b770658d
Pavel Platov <fenixkrd@bk.ru>
2023-07-28 00:26:06 +0300

    Initial Commit

---
+++ autolayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autolayout
+//
+//  Created by Павел Платов on 28.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
